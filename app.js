161bc260430d2bc2d219f2987f6febd659e82743
ern19 <aarontrammell19@gmail.com>
2017-09-12 15:18:50 -0400

    I have made a letter appear

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-console.log("brp")
+const words = ["element", "selector", "function", "variable", "loop", "jquery"]
+const defs = ["An HTML element is an individual component of an HTML document or web page.", "In CSS, selectors are patterns used to select the element(s) you want to style.", "A function is a reusuable block of code that does something in a script", "A variable is how you assign a value to something in JS", "Loops are a kind of function that carry out a block of code a specified number of times", "jQuery is a library of functions that allows developers to manipulate the DOM more efficiently"]
 
 $(document).ready(function(){
-    console.log("brp")
-    const words = ["element", "selector", "function", "variable", "loop", "jquery"]
-    const defs = ["An HTML element is an individual component of an HTML document or web page.", "In CSS, selectors are patterns used to select the element(s) you want to style.", "A function is a reusuable block of code that does something in a script", "A variable is how you assign a value to something in JS", "Loops are a kind of function that carry out a block of code a specified number of times", "jQuery is a library of functions that allows developers to manipulate the DOM more efficiently"]
+    $("#a").click(function() {
+        $(".blanks").html("a");
+    });
+    
 });
