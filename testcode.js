0b488f137d7e62162e8210276905653afab803be
ern19 <aarontrammell19@gmail.com>
2017-09-13 09:45:19 -0400

    added value attributes to letter buttons

--- testcode.js
+++ testcode.js
@@ -26,38 +26,44 @@
 const wordBank = [
     {
         word: "element",
+        blanks: "_______",
         definition: "An individual component of an HTML document or web page.",
     },
 
     {
         word: "selector",
+        blanks: "________",
         definition: " Are used to select the element(s) you want to style.",
     },
 
     {
         word: "function",
+        blanks: "________",
         definition: "A reusuable block of code that does something in a script",
     },
     
     {
         word: "variable",
+        blanks: "________",
         definition: "Something that you assign a value to.",
     },
 
     {
         word: "loop",
+        blanks: "____",
         definition: "A kind of function that carries out a block of code a specified # of times.",
     },
     
     {
         word: "jQuery",
+        blanks: "______",
         definition: "A library of functions that allows developers to manipulate the DOM more efficiently",
     }
 ]
 
 $("#startButton").click(function() {
     $("#hint").html(wordBank[0].definition)
-    $("#blanks").html(wordBank[0].word)
+    $("#blanks").html(wordBank[0].blanks)
 });
 
 
