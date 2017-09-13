792510efcf021289297faef0cb67bfef996f4e84
ern19 <aarontrammell19@gmail.com>
2017-09-12 20:46:40 -0400

    Linked start button to provide first hint

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const words = ["element", "selector", "function", "variable", "loop", "jquery"]
-const defs = ["An HTML element is an individual component of an HTML document or web page.", "In CSS, selectors are patterns used to select the element(s) you want to style.", "A function is a reusuable block of code that does something in a script", "A variable is how you assign a value to something in JS", "Loops are a kind of function that carry out a block of code a specified number of times", "jQuery is a library of functions that allows developers to manipulate the DOM more efficiently"]
-
+const defs = ["An individual component of an HTML document or web page.", "In CSS, selectors are patterns used to select the element(s) you want to style.", "A function is a reusuable block of code that does something in a script", "A variable is how you assign a value to something in JS", "Loops are a kind of function that carry out a block of code a specified number of times", "jQuery is a library of functions that allows developers to manipulate the DOM more efficiently"]
+// var letterContent = document.getElementsByClassName("letter").textContent;
 $(document).ready(function(){
-    $("#a").click(function() {
-        $(".blanks").html("a");
-    });
+    // let letterValue = $(".button").text
+    // $(".letter").click(function() {
+    //     $(".blanks").html(letterContent);
+    // });
     
+    $("#startButton").click(function() {
+        $("#hint").html(defs[0])
+
+    });
 });
