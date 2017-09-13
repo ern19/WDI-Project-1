82595e7a302aa6fff70aba036b0945cf05348aba
ern19 <aarontrammell19@gmail.com>
2017-09-13 15:53:50 -0400

    win state acheived

--- app.js
+++ app.js
@@ -3,54 +3,42 @@ const wordBank = [
         word: "element",
         blanks: [],
         definition: "An individual component of an HTML document or web page.",
-        wordToArray: function wordArray(word) {
-            word.split("")
-        }
+       
     },
 
     {
         word: "selector",
         blanks: [],
         definition: " Are used to select the element(s) you want to style.",
-        wordToArray: function wordArray(word) {
-            word.split("")
-        }
+        
     },
 
     {
         word: "function",
         blanks: [],
         definition: "A reusuable block of code that does something in a script",
-        wordToArray: function wordArray(word) {
-            word.split("")
-        }
+       
     },
     
     {
         word: "variable",
         blanks: [],
         definition: "Something that you assign a value to.",
-        wordToArray: function wordArray(word) {
-            word.split("")
-        }
+       
     },
 
     {
         word: "loop",
         blanks: [],
         definition: "A kind of function that carries out a block of code a specified # of times.",
-        wordToArray: function wordArray(word) {
-            word.split("")
-        }
+        
     },
     
     {
         word: "jQuery",
         blanks: [],
         definition: "A library of functions that allows developers to manipulate the DOM more efficiently",
-wordToArray: function wordArray(word) {
-            word.split("")
-        }
+
     }
 ]
 
@@ -74,13 +62,13 @@ $(document).ready(function(){
                 }
                 else {
                     console.log("Incorrect!")
-
+                    
                 }
-
             
         }
-    
-    
+        if (wordBank[0].blanks.length === wordBank[0].word.length) {
+            alert("You win!")
+        }
     });
 });
 
