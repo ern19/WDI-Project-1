671398e907a9d4a557382e87f911ccb08e8e9ff1
ern19 <aarontrammell19@gmail.com>
2017-09-13 11:34:41 -0400

    hardcore code cleanup in app.js

--- app.js
+++ app.js
@@ -53,47 +53,22 @@ wordToArray: function wordArray(word) {
         }
     }
 ]
-// var letterContent = document.getElementsByClassName("letter").textContent;
+
 $(document).ready(function(){
-    // let letterValue = $(".button").text
-    // $(".letter").click(function() {
-    //     $(".blanks").html(letterContent);
-    // });
+    $("#startButton").click(function() {
+        $("#hint").html(wordBank[4].definition)
+        $("#blanks").html(wordBank[4].blanks)
+    });
     
-//     $("#startButton").click(function() {
-        
-//         $("#hint").html(defs[0])
-//     $(".letter").click(function() { 
-//         console.log(textContent(".letter")) 
-    
-//     });
-//     });
-// });
-$("#startButton").click(function() {
-    $("#hint").html(wordBank[4].definition)
-    $("#blanks").html(wordBank[4].blanks)
-});
-});
-console.log(wordBank[1].definition);
-
-var wordArray = wordBank[1].word.split("")
-
-var buttonValue = $(".button").value
-//Upon clicking the letter button, the value of the button clicked is compared to the 
-//values contained within wordArray.
-//
-console.log(wordBank[1].wordToArray(wordBank[1].word))
-
-
-// $('.button').click(function(){
-//     console.log($(this).attr('value'));
-// });
-// $('.letter').click(function(){
-//     console.log($(this).attr('value'));
-// });
-
-$('.letter').click(function(){
+    $('.letter').click(function(){
     var letterVal = ($(this).attr('value'));
     console.log(letterVal)
+    }); 
 });
+
+
+
+
+
+
 
