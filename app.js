c32367c7bb8fa4d16174b813b5205e0cc2694c30
ern19 <aarontrammell19@gmail.com>
2017-09-14 15:22:20 -0400

    not really sure what happened last time

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const wordBank = [//all word/def pairs for gameplay
     },
     
     {
-        word: "jQuery",
+        word: "jquery",
         blanks: [],
         definition: "A library of functions that allows developers to manipulate the DOM more efficiently",
 
@@ -43,22 +43,26 @@ const wordBank = [//all word/def pairs for gameplay
 ]
 
 $(document).ready(function(){//start code
-    $("#startButton").click(function() {
-        $("#hint").html(wordBank[1].definition)
-        $("#blanks").html(wordBank[1].blanks)
+    let randWord = Math.floor(Math.random() * wordBank.length)
+    $("#startButton").click(function() {   
+          
+        $("#hint").html(wordBank[randWord].definition)
+        $("#blanks").html(wordBank[randWord].blanks)
     });
     
     $('.letter').click(function(){//comparison code
         var letterVal = ($(this).attr('value'));
         console.log(letterVal)
-        var wordArray = wordBank[1].word.split("");
+        var wordInPlay = wordBank[randWord].word
+        var blanksInPlay = wordBank[randWord].blanks
+        var wordArray = wordBank[randWord].word.split("");
         console.log(wordArray);
         
         for (var i = 0; i < wordArray.length; i++) {         
             if (letterVal === wordArray[i]) {
                     console.log("Correct!")
-                    wordBank[1].blanks.splice([i], 0, letterVal);
-                    $("#blanks").html(wordBank[1].blanks)
+                    blanksInPlay.splice([i], 0, letterVal);
+                    $("#blanks").html(blanksInPlay)
                 }
                 else {
                     console.log("Incorrect!")
@@ -73,8 +77,10 @@ $(document).ready(function(){//start code
     });
     $('.letter').click(function(){ //"scoreboard" code
         var letterVal = ($(this).attr('value'));
-        var wordLength = wordBank[1].word.length
-        var blankLength = wordBank[1].blanks.length
+        var wordLength = wordBank[randWord].word.length
+        var wordInPlay = wordBank[randWord].word
+        var blanksInPlay = wordBank[randWord].blanks
+        var blankLength = wordBank[randWord].blanks.length
         var winCount = $("#winCount")
         
         if (wordLength === blankLength) {
@@ -82,7 +88,7 @@ $(document).ready(function(){//start code
             winCount.html(+1)
             
         }
-        if (!wordBank[1].word.includes(letterVal)) {
+        if (!wordInPlay.includes(letterVal)) { //failed attempts
             var failCount = document.getElementById('failCount');
             var number = failCount.innerHTML;
             number++;
@@ -99,8 +105,11 @@ $(document).ready(function(){//start code
         }
     });
     $('.letter').click(function(){
-        console.log("a button was pressed")
+       console.log("a button was pressed")
+    //    $('.letter').css("color", "red");
+
     });
+
 });
 
 
