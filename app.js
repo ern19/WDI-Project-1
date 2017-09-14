6f63c98ca02ad1ee804c903a22ca203ee53a91f8
ern19 <aarontrammell19@gmail.com>
2017-09-14 15:18:15 -0400

    gameplay now starts with random word

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const wordBank = [//all word/def pairs for gameplay
 $(document).ready(function(){//start code
     let randWord = Math.floor(Math.random() * wordBank.length)
     $("#startButton").click(function() {   
-        let randWord = Math.floor(Math.random() * wordBank.length)  
+          
         $("#hint").html(wordBank[randWord].definition)
         $("#blanks").html(wordBank[randWord].blanks)
     });
@@ -83,13 +83,8 @@ $(document).ready(function(){//start code
         var blankLength = wordBank[randWord].blanks.length
         var winCount = $("#winCount")
         
-<<<<<<< HEAD
-        if (wordLength === blankLength) {//wins
-            alert("You win!")
-=======
         if (wordLength === blankLength) {
             setTimeout(function() { $("#hint").html("You win!") }, 1500);
->>>>>>> c895edfcccbd94e11df3cc87312d2278165f3002
             winCount.html(+1)
             
         }
@@ -99,13 +94,8 @@ $(document).ready(function(){//start code
             number++;
             failCount.innerHTML = number;
         }
-<<<<<<< HEAD
-        if (number === 6) { //body count
-            alert("he ded")
-=======
         if (number === 6) {
             setTimeout(function() { $("#hint").html("You lost :(") }, 1500);
->>>>>>> c895edfcccbd94e11df3cc87312d2278165f3002
             $(".start").hide()
             $(".lose").show()
             var bodyCount = document.getElementById('bodyCount');
