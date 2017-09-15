c6c37348bb941bcc758527131c469a2582a5b938
ern19 <aarontrammell19@gmail.com>
2017-09-15 09:00:44 -0400

    Buttons reactivate when word is completed, or if attempts are exhausted

--- app.js
+++ app.js
@@ -2,42 +2,42 @@ const wordBank = [//all word/def pairs for gameplay
     {
         word: "element",
         blanks: [],
-        definition: "An individual component of an HTML document or web page.",
+        definition: "An individual component of an HTML document or web page. (7 letters)",
        
     },
 
     {
         word: "selector",
         blanks: [],
-        definition: " Are used to select the element(s) you want to style.",
+        definition: "Are used to select the element(s) you want to style. (8 letters)",
         
     },
 
     {
         word: "function",
         blanks: [],
-        definition: "A reusuable block of code that does something in a script",
+        definition: "A reusuable block of code that does something in a script (8 letters)",
        
     },
     
     {
         word: "variable",
         blanks: [],
-        definition: "Something that you assign a value to.",
+        definition: "Something that you assign a value to. (8 letters)",
        
     },
 
     {
         word: "loop",
         blanks: [],
-        definition: "A kind of function that carries out a block of code a specified # of times.",
+        definition: "A kind of function that carries out a block of code a specified # of times. (4 letters)",
         
     },
     
     {
         word: "jquery",
         blanks: [],
-        definition: "A library of functions that allows developers to manipulate the DOM more efficiently",
+        definition: "A library of functions that allows developers to manipulate the DOM more efficiently. (6 letters)",
 
     }
 ]
@@ -100,18 +100,21 @@ $(document).ready(function(){//start code
         var winCount = $("#winCount")
         
         if (wordLength === blankLength) {
+            $('.letter').prop("disabled", false)
             setTimeout(function() { $("#hint").html("You win!") }, 1500);
             winCount.html(+1)
-            $("#nextButton").show()
-            
+            $("#nextButton").show()          
         }
+
         if (!wordInPlay.includes(letterVal)) { //failed attempts
             var failCount = document.getElementById('failCount');
             var number = failCount.innerHTML;
             number++;
             failCount.innerHTML = number;
         }
+
         if (number === 6) {
+            $('.letter').prop("disabled", false)
             setTimeout(function() { $("#hint").html("You lost :(") }, 1500);
             $(".start").hide()
             $(".lose").show()
@@ -119,9 +122,10 @@ $(document).ready(function(){//start code
             var number = bodyCount.innerHTML;
             number++;
             bodyCount.innerHTML = number;
-            $("#nextButton").show()
+            $("#nextButton").show()   
         }
     });
+    
     $('.letter').click(function(){
        console.log("a button was pressed")
     //    $('.letter').css("color", "red");
